98a7b94c43c6f26bd9f68432bd3ce68f8cb49c5d
Vansh Kumar <vanshkumar.mail@gmail.com>
2023-07-22 11:45:11 +0530

    done with the code

--- test4/index.php
+++ test4/index.php
@@ -16,81 +16,9 @@
             font-size: 24px;
         }
     </style>
-    <script>
-        function displayPDFAsImage(url, callback) {
-            const loadingTask = pdfjsLib.getDocument(url);
-            loadingTask.promise.then(function (pdf) {
-                pdf.getPage(1).then(function (page) {
-                    const scale = 1;
-                    const viewport = page.getViewport({ scale });
 
-                    const img = document.createElement('img');
-                    const canvas = document.createElement('canvas');
-                    const context = canvas.getContext('2d');
-                    canvas.height = viewport.height;
-                    canvas.width = viewport.width;
 
-                    page.render({ canvasContext: context, viewport: viewport }).promise.then(function () {
-                        img.src = canvas.toDataURL('image/jpeg', 0.7);
-                        img.className = 'h-36 w-full overflow-hidden';
-
-                        const pdfContainer = document.getElementById('pdf-img');
-                        pdfContainer.appendChild(img);
-
-                        // Call the callback function to indicate that the rendering is complete
-                        if (typeof callback === 'function') {
-                            callback();
-                        }
-                    }).catch(function (error) {
-                        console.error('Error rendering PDF as image:', error);
-                    });
-                }).catch(function (error) {
-                    console.error('Error loading PDF:', error);
-                });
-            }).catch(function (error) {
-                console.error('Error loading PDF:', error);
-            });
-        }
-
-        function saveImageToServer() {
-            const pdfContainer = document.getElementById('pdf-img');
-            const img = pdfContainer.querySelector('img');
-
-            const imageData = img.src.split(',')[1];
-
-            const xhr = new XMLHttpRequest();
-            xhr.open('POST', 'save_image.php', true);
-            xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        alert('Image saved successfully.');
-                    } else {
-                        alert('Failed to save image.');
-                    }
-                }
-            };
-            xhr.send('imageData=' + encodeURIComponent(imageData));
-        }
-
-        function uploadPDF() {
-            const fileInput = document.getElementById('pdf-file');
-            const file = fileInput.files[0];
-            if (file) {
-                const fileReader = new FileReader();
-                fileReader.onload = function (event) {
-                    const pdfData = event.target.result;
-                    // Call the displayPDFAsImage() with the saveImageToServer() function as a callback
-                    displayPDFAsImage(pdfData, saveImageToServer);
-                };
-                fileReader.readAsDataURL(file);
-            } else {
-                alert('Please select a PDF file.');
-            }
-        }
-
-    </script>
-
+<script src="./script.js"></script>
 </head>
 
 <body>
@@ -126,12 +54,6 @@
             </div>
         </div>
     </div>
-
-    <!-- Input for file upload -->
-    <input type="file" id="pdf-file" accept=".pdf" />
-    <!-- Button to trigger manual PDF upload -->
-    <button onclick="uploadPDF()">Upload PDF</button>
-
 </body>
 
 </html>
---
+++ test4/upload.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>PDF File Upload</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <script src="https://mozilla.github.io/pdf.js/build/pdf.js"></script>
+    <style>
+        .container {
+            width: 500px;
+            margin: 50px auto;
+        }
+
+        h1 {
+            text-align: center;
+            font-size: 24px;
+        }
+    </style>
+    <script src="./script.js"></script>
+</head>
+
+<body>
+    <!-- Input for file upload -->
+    <input type="file" id="pdf-file" accept=".pdf" />
+    <!-- Button to trigger manual PDF upload -->
+    <button onclick="uploadPDF()">Upload PDF</button>
+    <div id="pdf-img" class="pdf-container"></div>
+</body>
+
+</html>
